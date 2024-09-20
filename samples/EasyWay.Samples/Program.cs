11bd6b6690401e601b218485c12cf793a98de05a
adimiko <amikolajczyk.it@gmail.com>
2024-09-20 18:19:16 +0200

    Added PostgreSql and EntityFrameworkCore to sample

--- samples/EasyWay.Samples/Program.cs
+++ samples/EasyWay.Samples/Program.cs
@@ -1,6 +1,14 @@
 using EasyWay;
 using EasyWay.EntityFrameworkCore;
 using EasyWay.Samples;
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.PostgreSql;
+
+var postgreSqlContainer = new PostgreSqlBuilder()
+  .WithImage("postgres:15.1")
+  .Build();
+
+await postgreSqlContainer.StartAsync();
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +18,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddEasyWay(typeof(SampleCommand).Assembly);
+builder.Services.AddEntityFrameworkCore<SampleDbContext>(x => x.UseNpgsql(postgreSqlContainer.GetConnectionString()));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+
+    scope.ServiceProvider.GetRequiredService<SampleDbContext>().Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ samples/EasyWay.Samples/SampleDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EasyWay.Samples
+{
+    public  class SampleDbContext : DbContext
+    {
+        public SampleDbContext() : base() { }
+
+        public SampleDbContext(DbContextOptions<SampleDbContext> options)
+            : base(options) { }
+    }
+}
