89b2e27b250260a16eabe86d1de42a22adff6fde
adimiko <amikolajczyk.it@gmail.com>
2024-09-26 16:30:16 +0200

    Added public contract test for AggregateRoot

---
+++ tests/EasyWay.Tests/AggregateRoots/AggregateRootPublicContractTests.cs
@@ -0,0 +1,19 @@
+﻿using EasyWay.Tests.SeedWorks;
+
+namespace EasyWay.Tests.AggregateRoots
+{
+    public sealed class AggregateRootPublicContractTests
+    {
+        private readonly Type _aggregateRootType = typeof(AggregateRoot);
+
+        [Fact(DisplayName = $"{nameof(AggregateRoot)} public contract test")]
+        public void PublicContractTest()
+        {
+            _aggregateRootType
+                .IsPublic()
+                .IsAbstract()
+                .InheritanceFrom(typeof(Entity))
+                .HasFullName("EasyWay.AggregateRoot");
+        }
+    }
+}
---
+++ tests/EasyWay.Tests/SeedWorks/TypeChecker.cs
@@ -0,0 +1,40 @@
+﻿namespace EasyWay.Tests.SeedWorks
+{
+    internal static class TypeChecker
+    {
+        public static Type IsPublic(this Type type)
+        {
+            Assert.True(type.IsPublic, $"{type.Name} must be public class");
+
+            return type;
+        }
+
+        public static Type IsSealed(this Type type)
+        {
+            Assert.True(type.IsSealed, $"{type.Name} must be sealed class");
+
+            return type;
+        }
+
+        public static Type IsAbstract(this Type type)
+        {
+            Assert.True(type.IsAbstract, $"{type.Name} must be abstract class");
+
+            return type;
+        }
+
+        public static Type HasFullName(this Type type, string fullNameType)
+        {
+            Assert.True(fullNameType == type.FullName, "Incorrect full name type");
+
+            return type;
+        }
+
+        public static Type InheritanceFrom(this Type type, Type baseClass)
+        {
+            Assert.True(type.IsSubclassOf(baseClass), $"{type.Name} must inheritance from {baseClass.Name}");
+
+            return type;
+        }
+    }
+}
