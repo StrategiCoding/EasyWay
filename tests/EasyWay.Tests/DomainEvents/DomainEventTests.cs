1de30e3354ad6a7c4d3694cbc017b3c44358ef33
adimiko <amikolajczyk.it@gmail.com>
2024-09-26 08:30:44 +0200

    Added test for DomainEvent

---
+++ tests/EasyWay.Tests/DomainEvents/DomainEventTests.cs
@@ -0,0 +1,24 @@
+﻿namespace EasyWay.Tests.DomainEvents
+{
+    public sealed class DomainEventTests
+    {
+        internal sealed class TestDomainEvent : DomainEvent;
+
+        [Fact(DisplayName = $"Domain event should create with correct '{nameof(DomainEvent.EventId)}' and '{nameof(DomainEvent.OccurrenceOn)}'")]
+        public void Test()
+        {
+            // Arrange
+            var expectedDateTime = DateTime.UtcNow;
+            var precision = TimeSpan.FromMilliseconds(50);
+
+            Clock.Set(expectedDateTime);
+
+            // Act
+            var domainEvent = new TestDomainEvent();
+
+            // Assert
+            Assert.NotEqual(Guid.Empty, domainEvent.EventId);
+            Assert.Equal(expectedDateTime, domainEvent.OccurrenceOn, precision);
+        }
+    }
+}
