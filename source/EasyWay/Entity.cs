6dad6561d6d307ef6385d272c57d74fd473fb531
adimiko <amikolajczyk.it@gmail.com>
2024-09-27 08:51:51 +0200

    Implemented IEquatable and IEqualityComparer for Entity

--- source/EasyWay/Entity.cs
+++ source/EasyWay/Entity.cs
@@ -1,10 +1,11 @@
 ﻿using EasyWay.Internals.BusinessRules;
 using EasyWay.Internals.DomainEvents;
 using EasyWay.Internals.IdGenerators;
+using System.Diagnostics.CodeAnalysis;
 
 namespace EasyWay
 {
-    public abstract class Entity
+    public abstract class Entity : IEquatable<Entity>, IEqualityComparer<Entity>
     {
         internal Guid Id { get; private set; } = IdGenerator.New;
 
@@ -35,20 +36,25 @@ namespace EasyWay
             _domainEvents.Add(domainEvent);
         }
 
-        public sealed override bool Equals(object? obj)
-        {
-            if (obj is not null && obj is Entity entity) 
-            { 
-                return Id == entity.Id;
-            }
+        public bool Equals(Entity? other) => other is not null ? Id == other.Id : false;
 
-            return false;
-        }
+        public sealed override bool Equals(object? obj) => obj is Entity entity ? Equals(entity) : false;
 
         public static bool operator ==(Entity x, Entity y) => x.Id == y.Id;
 
         public static bool operator !=(Entity x, Entity y) => x.Id != y.Id;
 
         public sealed override int GetHashCode() => Id.GetHashCode();
+
+        public bool Equals(Entity? x, Entity? y)
+        {
+            if (x is null && y is null) return true;
+            if (x is null || y is null) return false;
+            if (x.Id == y.Id) return true;
+
+            return false;
+        }
+
+        public int GetHashCode([DisallowNull] Entity obj) => obj.Id.GetHashCode();
     }
 }
