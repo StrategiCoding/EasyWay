dd329664dbc1ad0f3cef30505234aa44b1288ba5
adimiko <amikolajczyk.it@gmail.com>
2024-10-28 20:35:09 +0100

    save

---
+++ source/EasyWay.Auth/Internals/RefreshTokenCreators/IRefreshTokenHasher.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyWay.Internals.RefreshTokenCreators
+{
+    internal interface IRefreshTokenHasher
+    {
+        byte[] Salt();
+
+        string Hash(string refreshToken, byte[] salt);
+
+        bool Verify(string refreshToken, byte[] salt, string hashedRefreshToken);
+    }
+}
---
+++ source/EasyWay.Auth/Internals/RefreshTokenCreators/RefreshTokenHasher.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+
+namespace EasyWay.Internals.RefreshTokenCreators
+{
+    internal sealed class RefreshTokenHasher : IRefreshTokenHasher
+    {
+        const int KeySize = 256 / 8;
+
+        const int Iterations = 3;
+
+        private readonly HashAlgorithmName AlgorithmName = HashAlgorithmName.SHA256;
+
+        public byte[] Salt() => RandomNumberGenerator.GetBytes(KeySize);
+
+        public string Hash(string refreshToken, byte[] salt)
+        {
+            var key = Rfc2898DeriveBytes.Pbkdf2(refreshToken, salt, Iterations, AlgorithmName, KeySize);
+
+            return Convert.ToBase64String(key);
+        }
+
+        public bool Verify(string refreshToken, byte[] salt, string hashedRefreshToken)
+        {
+            return Convert.FromBase64String(hashedRefreshToken) == Rfc2898DeriveBytes.Pbkdf2(refreshToken, salt, Iterations, AlgorithmName, KeySize);
+        }
+    }
+}
