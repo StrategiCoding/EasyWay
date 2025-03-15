1dbc6c68643c1ee4e1c82ea6fd252ecfd8d703b5
adimiko <amikolajczyk.it@gmail.com>
2025-03-15 14:51:45 +0100

    Added new query results

--- source/EasyWay/QueryResult.cs
+++ source/EasyWay/QueryResult.cs
@@ -5,33 +5,53 @@ namespace EasyWay
     public sealed class QueryResult<TReadModel>
         where TReadModel : ReadModel
     {
-        internal TReadModel ReadModel { get;  }
+        internal TReadModel? ReadModel { get;  }
 
         internal QueryErrorEnum Error { get; }
 
         internal IDictionary<string, string[]> ValidationErrors;
 
+        internal Exception? Exception { get; }
+
         private QueryResult(TReadModel readModel)
         {
             ReadModel = readModel;
+            ValidationErrors = new Dictionary<string, string[]>();
             Error = QueryErrorEnum.None;
+            Exception = null;
         }
 
         private QueryResult(QueryErrorEnum queryError)
         {
             ReadModel = null;
+            ValidationErrors = new Dictionary<string, string[]>();
             Error = queryError;
+            Exception = null;
         }
 
         private QueryResult(IDictionary<string, string[]> validationErrors)
         {
+            ReadModel = null;
             ValidationErrors = validationErrors;
             Error = QueryErrorEnum.Validation;
+            Exception = null;
         }
 
-        public static QueryResult<TReadModel> Ok(TReadModel readModel) => new QueryResult<TReadModel>(readModel);
+        private QueryResult(Exception exception)
+        {
+            ReadModel = null;
+            ValidationErrors = new Dictionary<string, string[]>();
+            Error = QueryErrorEnum.UnknownException;
+            Exception = exception;
+        }
 
         internal static QueryResult<TReadModel> Validation(IDictionary<string, string[]> validationErrors) => new QueryResult<TReadModel>(validationErrors);
+
+        internal static QueryResult<TReadModel> OperationCanceled() => new QueryResult<TReadModel>(QueryErrorEnum.OperationCanceled);
+
+        internal static QueryResult<TReadModel> UnknownException(Exception exception) => new QueryResult<TReadModel>(exception);
+
+        public static QueryResult<TReadModel> Ok(TReadModel readModel) => new QueryResult<TReadModel>(readModel);
 
         public static QueryResult<TReadModel> NotFound => new QueryResult<TReadModel>(QueryErrorEnum.NotFound);
 
